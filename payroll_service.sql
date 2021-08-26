4a1e3c8d718b958e9c374845bd329d5a0dd3a26d
bharathmighty248 <bharathmighty248@gmail.com>
2021-08-26 22:11:08 +0530

    [Bharath] Add:Adding Gender To Employee Implemented

--- payroll_service.sql
+++ payroll_service.sql
@@ -16,6 +16,17 @@ insert into employee_payroll (Name, Salary, Startdate) values ('Nagoor', '35000'
 
 Select * from employee_payroll
 
-Select Salary from employee_payroll where Name='bharath'
+Select Salary from employee_payroll where Name='Bharath'
 
 Select Salary from employee_payroll where Startdate between ('26-5-2021') and ('26-6-2021')
+
+Alter Table employee_payroll
+Add Gender varchar(1)
+
+Update employee_payroll 
+set Gender ='M' where Name='Bharath' or Name='Sriram' or Name='Nagoor'
+Update employee_payroll 
+set Gender='F' where name='Susmitha'
+
+Select * from employee_payroll
+
