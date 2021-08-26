445f69fafe381b4d65923fe8be25d0b7028e43b9
bharathmighty248 <bharathmighty248@gmail.com>
2021-08-26 18:59:51 +0530

    [Bharath]Add:employee_payroll Table Creation Implemented

--- payroll_service.sql
+++ payroll_service.sql
@@ -1,3 +1,10 @@
 create database payroll_service
 
 use payroll_service
+
+Create Table employee_payroll (
+Id int identity (1,1) primary key,
+Name varchar(20),
+Salary varchar(20),
+Startdate varchar(20)
+)
