12012b5854376954ba15433780c36de6089b2193
bharathmighty248 <bharathmighty248@gmail.com>
2021-08-26 19:11:01 +0530

    [Bharath] Add:Creation Of Employees Payroll Data Implemented

--- payroll_service.sql
+++ payroll_service.sql
@@ -8,3 +8,8 @@ Name varchar(20),
 Salary varchar(20),
 Startdate varchar(20)
 )
+
+insert into employee_payroll (Name, Salary, Startdate) values ('Bharath', '50000', '26-5-2021')
+insert into employee_payroll (Name, Salary, Startdate) values ('Susmitha', '45000', '26-6-2021')
+insert into employee_payroll (Name, Salary, Startdate) values ('Sriram', '40000', '26-7-2021')
+insert into employee_payroll (Name, Salary, Startdate) values ('Nagoor', '35000', '26-8-2021')
